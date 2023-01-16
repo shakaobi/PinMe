98d443ffbf90865b478c477186842640a884209b
Camesha Goods <camesha.goods1@gmail.com>
2023-01-15 21:57:28 -0500

    made a pindetailMorePin query in Utils.

--- src/components/PinDetail.jsx
+++ src/components/PinDetail.jsx
@@ -6,7 +6,11 @@ import {v4 as uuidv4} from 'uuid'
 import {client, urlFor}from '../container/client';
 import MasonryLayout from './MasonryLayout';
 import {pinDetailMorePinQuery, pinDetailQuery} from '../utils/data';
+import Spinner from './Spinner';
+
 const PinDetail = ({user}) => {
+  const {pinId} = useParams();
+  
   return (
     <div>PinDetail</div>
   )
--- src/utils/data.js
+++ src/utils/data.js
@@ -87,7 +87,31 @@ export const pinDetailQuery = (pinId) => {
     }`;
     return query;
 };
-export const pinDetailMorePinQuery = (pin)
+export const pinDetailMorePinQuery = (pin) => {
+    const query = `*[_type == 'pin'&& category == '${pin.category}' &&_id != '${pin._id}']{
+        image{
+            asset->{
+                url
+            }
+        },
+        _id,
+        destination,
+        postedBy->{
+            _id,
+            userName,
+            image
+        },
+        save[]{
+            _key,
+            postedBy->{
+                _id,
+                userName,
+                image
+            },
+        },
+    }`;
+    return query
+};
 export const searchQuery = (searchTerm) => {
     const query =`*[_type == 'pin' && title match '${searchTerm}*' || category match '${searchTerm}*' || about match '${searchTerm}*' ]{
         image {
