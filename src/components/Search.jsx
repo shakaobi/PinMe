5ef71eaf0e5667eed6472bb4f41ea8230d2276b7
Camesha Goods <camesha.goods1@gmail.com>
2023-01-16 22:33:44 -0500

    Working on search bar.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,37 @@
 import React,{useState, useEffect} from 'react';
-import
-const Search = () => {
+import MasonryLayout from './MasonryLayout';
+import {client} from '../container/client'
+import {feedQuery, searchQuery} from '../utils/data'
+import Spinner from './Spinner';
+
+const Search = ({searchTerm}) => {
+  const [pins, setPins] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if(searchTerm !== '') {
+      setLoading(true);
+      const query = searchQuery(searchTerm.toLowerCase());
+      client.fetch(query).then((data)=>{
+        setPins(data);
+        setLoading(false);
+      })
+    }else{
+      client.fetch(feedQuery).then((data)=>{
+        setPins(data);
+        setLoading(false);
+      })
+    }
+  }, [searchTerm])
+  
   return (
     <div>
-      
-    </div>
-  )
-}
+      {loading && <Spinner message='search pins'/>}
+      {pins?.length !== 0 && <MasonryLayout pins={pins}/>}
+      {pins?.length === 0 && searchTerm !== '' && !loading &&(
+        <div className='mt-10 text-center text-xl'> No Pins Found!</div>)}
+      </div>
+  );
+};
 
 export default Search;
